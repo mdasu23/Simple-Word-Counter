a64dfaa962c60bdcfe1a4fbb701c7d41efcb6940
mdasu23 <d.m.anuraag@gmail.com>
2023-12-21 20:57:08 +0000

    Final Commit

--- Makefile
+++ Makefile
@@ -1,16 +1,9 @@
-all: main
+# Makefile for ccwc
 
-CXX = clang++
-override CXXFLAGS += -g -Wmost -Werror
+CC = g++
+CFLAGS = -Wall -Wextra -std=c++11
 
-SRCS = $(shell find . -name '.ccls-cache' -type d -prune -o -type f -name '*.cpp' -print | sed -e 's/ /\\ /g')
-HEADERS = $(shell find . -name '.ccls-cache' -type d -prune -o -type f -name '*.h' -print)
-
-main: $(SRCS) $(HEADERS)
-	$(CXX) $(CXXFLAGS) $(SRCS) -o "$@"
-
-main-debug: $(SRCS) $(HEADERS)
-	$(CXX) $(CXXFLAGS) -U_FORTIFY_SOURCE -O0 $(SRCS) -o "$@"
-
+ccwc: ccwc.cpp
+		$(CC) $(CFLAGS) -o ccwc ccwc.cpp
 clean:
-	rm -f main main-debug
+		rm -f ccwc
--- ccwc.cpp
+++ ccwc.cpp
@@ -1,38 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int countbytesinfile(char* filename) {
-  ifstream input_file(filename);
-  if(!input_file.is_open()) {
-    cerr<<"Error: Could not open the file "<<filename<<endl;
-    return -1;
-  }
+int countbytesinfile(ifstream &input_file) {
   input_file.seekg(0,ios::end);
   int size=input_file.tellg();
-  input_file.close();
   return size;
 }
 
-int countlinesinfile(char* filename) {
-  ifstream input_file(filename);
-  if(!input_file.is_open()) {
-    cerr<<"Error: Could not open the file "<<filename<<endl;
-    return -1;
-  }
+int countlinesinfile(ifstream &input_file) {
   int numlines=0; string line;
   while(getline(input_file,line)) {
     numlines++;
   }
-  input_file.close();
   return numlines;
 }
 
-int countwordsinfile(char* filename) {
-  ifstream input_file(filename);
-  if(!input_file.is_open()) {
-    cerr<<"Error: Could not open the file "<<filename<<endl;
-    return -1;
-  }
+int countwordsinfile(ifstream &input_file) {
   int numwords=0; string line;
   while(getline(input_file,line)) {
     string word; istringstream ss(line);
@@ -40,70 +23,106 @@ int countwordsinfile(char* filename) {
       numwords++;
     }
   }
-  input_file.close();
   return numwords;
 }
 
-int countcharsinfile(char* filename) {
-  ifstream input_file(filename);
-  if(!input_file.is_open()) {
-    cerr<<"Error: Could not open the file "<<filename<<endl;
-    return -1;
-  }
+int countcharsinfile(ifstream &input_file) {
   int numchars=0; char ch;
   while(input_file.get(ch)) {
     numchars++;
   }
-  input_file.close();
   return numchars;
 }
 
+ifstream openfile(char* filename) {
+  ifstream input_file(filename);
+  if(!input_file.is_open()) {
+    cerr<<"Error: Could not open the file "<<filename<<endl;
+    return ifstream();
+  }
+  return input_file;
+}
+
 int main(int argc, char* argv[]) {
+  string cincheck; getline(cin,cincheck);
+  if(cincheck.length()!=0) {
+      string line; ofstream output_file("data.txt");
+      while(getline(cin,line)) {
+        output_file<<line<<endl;
+      }
+      output_file.close();
+      ifstream input_file("data.txt");
+      if(strcmp(argv[1],"-c")==0) {
+      int numbytes=countbytesinfile(input_file);
+        if(numbytes!=-1) {
+          cout<<numbytes<<endl;
+        }
+      }
+      if(strcmp(argv[1],"-l")==0) {
+        int numlines=countlinesinfile(input_file);
+        if(numlines!=-1) {
+          cout<<numlines<<endl;
+        }
+      }  
+      if(strcmp(argv[1],"-w")==0) {
+        int numwords=countwordsinfile(input_file);
+        if(numwords!=-1) {
+          cout<<numwords<<endl;
+        }
+      }  
+      if(strcmp(argv[1],"-m")==0) {
+        int numchars=countcharsinfile(input_file);
+        if(numchars!=-1) {
+          cout<<numchars<<endl;
+        }
+      }
+    input_file.close();
+    remove("data.txt");
+    return 0;
+  }
   if(argc==3) {
+    ifstream input_file=openfile(argv[2]);
     if(strcmp(argv[1],"-c")==0) {
-      int numbytes=countbytesinfile(argv[2]);
+      int numbytes=countbytesinfile(input_file);
       if(numbytes!=-1) {
         cout<<numbytes<<" "<<argv[2]<<endl;
         return 0;
       }
       return -1;
     } else if(strcmp(argv[1],"-l")==0) {
-      int numlines=countlinesinfile(argv[2]);
+      int numlines=countlinesinfile(input_file);
       if(numlines != -1) {
         cout<<numlines<<" "<<argv[2]<<endl;
         return 0;
       }
       return -1;
     } else if(strcmp(argv[1],"-w")==0) {
-      int numwords=countwordsinfile(argv[2]);
+      int numwords=countwordsinfile(input_file);
       if(numwords != -1) {
         cout<<numwords<<" "<<argv[2]<<endl;
         return 0;
       }
     } else if(strcmp(argv[1],"-m")==0) {
-        int numchars=countcharsinfile(argv[2]); // check again
+        int numchars=countcharsinfile(input_file); // check again
         if(numchars != -1) {
           cout<<numchars<<" "<<argv[2]<<endl;
           return 0;
         }
       return -1;
     }
+    input_file.close();
   } else if(argc==2) {
-    if(strcmp(argv[1],"-l")!=0 && strcmp(argv[1],"-m")!=0 && strcmp(argv[1],"-c")!=0) {
-      int numbytes=countbytesinfile(argv[1]);
-      int numlines=countlinesinfile(argv[1]);
-      int numwords=countwordsinfile(argv[1]);
-      if(numbytes != -1 && numlines != -1 && numwords != -1) {
-        cout<<numlines<<" "<<numwords<<" "<<numbytes<<" "<<argv[1]<<endl;
-        return 0;
-      } else {
-        cerr<<"Error: Error in opening file"<<endl;
-        return -1;
-      }
-    } else {
-      
-    }
+      ifstream input_file=openfile(argv[1]);
+      int numbytes=countbytesinfile(input_file);
+      input_file.close();
+      input_file=openfile(argv[1]);
+      int numlines=countlinesinfile(input_file);
+      input_file.close();
+      input_file=openfile(argv[1]);
+      int numwords=countwordsinfile(input_file);
+      input_file.close();
+      cout<<numlines<<" "<<numwords<<" "<<numbytes<<" "<<argv[1]<<endl;
+      return 0;
   } 
   return 0;
 }
-//check numchars // wrong
